a6811fd5f54a08b41dd176f9579550625c3e3399
Dario Torrejon <dartorrejon432@outlook.com>
2021-03-22 18:18:11 -0300

    Empezamos con la Clase2

---
+++ Clase2POO/src/com/educacionit/clases/Alumno.java
@@ -0,0 +1,25 @@
+package com.educacionit.clases;
+
+public class Alumno {
+	//Definimos los atributos de la clase
+	public String apellido;
+	public String nombre;
+	public int edad;
+	
+	
+	public String toString() {
+		return "Alumno [apellido=" + apellido + ", nombre=" + nombre + ", edad=" + edad + "]";
+	}
+	
+//	public String muestraEstadoDeAtributos() {
+//		return  "Nombre "+nombre+", "+"Apellido: "+apellido+"\n";
+//		
+//	}
+
+	
+	
+
+	
+	
+}
+
---
+++ Clase2POO/src/com/educacionit/main/AppPrincipal.java
@@ -0,0 +1,42 @@
+package com.educacionit.main;
+
+import com.educacionit.clases.Alumno;
+
+public class AppPrincipal {
+	public static void main(String[] args) {
+		Alumno alu1 = new Alumno();
+		Alumno alu2 = new Alumno();
+		Alumno alu3 = new Alumno();
+
+		
+		alu1.nombre = "Dario";
+		alu1.apellido = "Torrejon";
+		alu1.edad = 13;
+		
+		alu2.nombre = "Pablo";
+		alu2.apellido = "Torrejon";
+		alu2.edad = 33;
+		
+		alu3.nombre = "Maria";
+		alu3.apellido = "Rordiguez";
+		alu3.edad = 21;
+				
+
+//		// Mostramos por consola el estado de los atributos del objeto alu1
+//		System.out.println("Apellido: " + alu1.apellido);
+//		System.out.println("Nombre: " + alu1.nombre);
+//
+//		// Mostramos por consola el estado de los atributos del objeto alu2
+//		System.out.println("Apellido: " + alu2.apellido);
+//		System.out.println("Nombre: " + alu2.nombre);
+//
+//		// Mostramos por consola el estado de los atributos del objeto alu3
+//		System.out.println("Apellido: " + alu3.apellido);
+//		System.out.println("Nombre: " + alu3.nombre);
+
+		
+		System.out.println("Alumno 1 "+alu1.toString());
+		System.out.println("Alumno 2 "+alu2.toString());
+		System.out.println("Alumno 3 "+alu3.toString());
+	}
+}
