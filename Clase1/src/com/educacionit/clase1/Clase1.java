4ef0fc30604c6c6b52d9d730568483c804b58c10
Dario Torrejon <dartorrejon432@outlook.com>
2021-03-22 15:59:50 -0300

    Agregamos contenido a Clase1

--- Clase1/src/com/educacionit/clase1/Clase1.java
+++ Clase1/src/com/educacionit/clase1/Clase1.java
@@ -3,5 +3,6 @@ package com.educacionit.clase1;
 public class Clase1 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
+		System.out.println("Probando si git reconoce los cambios!");
 	}
 }
